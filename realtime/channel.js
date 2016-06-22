829ecb951551125fab78b9845f55809514198355
xmc1993 <1170143845@qq.com>
2016-06-22 16:24:21 +0800

    发送多人信息

--- realtime/channel.js
+++ realtime/channel.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
     , consts = require('./consts')
-    // , node_utils = require('../util/node_utils')
+// , node_utils = require('../util/node_utils')
     , caches = require('./caches')
     , redis = require('redis')
     , _ = require('underscore');
@@ -16,10 +16,10 @@ var onlineChannels = exports.onlineChannels = {};
  */
 exports.sendMessageToPeer = function (message, toPeerId) {  //此处的peerId应该就是RoleId
     var chs = peerChannels[toPeerId];
-    if(_.isArray(chs)){ //如果这人个在线
+    if (_.isArray(chs)) { //如果这人个在线
         _.each(chs, function (chId) {
             var ch = onlineChannels[chId];
-            if(ch & ch.connected){
+            if (ch & ch.connected) {
                 try {
                     ch.emit('message', message);
                 } catch (e) {
@@ -39,19 +39,18 @@ exports.sendMessageToMultiplePeers = function (message, toPeerIds) {
 };
 
 
-
 /**
  * 发送Group消息
  */
-exports.sendMessageToGroup = function (message, roleId, groupId){
+exports.sendMessageToGroup = function (message, roleId, groupId) {
     return new Promise(function (resolve, reject) {
         caches.ifPeerInGroup(roleId, groupId)
             .then(function (res) {
-                if(res){  //如果给Group发送信息的人在Group中 则给Group中所有在线的人员发送消息
+                if (res) {  //如果给Group发送信息的人在Group中 则给Group中所有在线的人员发送消息
                     var groupPeers = caches.getGroupMemberRoleIds(groupId);
                     exports.sendMessageToMultiplePeers(message, groupPeers);
                     resolve('ok');
-                }else{
+                } else {
                     reject(new Error('No Permission: 用户不在当前Group中'));
                 }
             }, function (error) {
@@ -70,17 +69,17 @@ exports.handleNewChannel = function (socket) {
         //设置socket的关联peerId
         socket.peerId = peerId;
         //将用户新的在线channel记录到peerChannels中
-       if(peerId){
-           var chs = peerChannels[peerId];
-           if(!_.isArray(chs)){
-               peerChannels[peerId] = [socket.id];
-           }else{
-               if(chs.indexOf(socket.id) < 0){
-                   peerChannels[peerId].push(socket.id);
-               }
-           }
+        if (peerId) {
+            var chs = peerChannels[peerId];
+            if (!_.isArray(chs)) {
+                peerChannels[peerId] = [socket.id];
+            } else {
+                if (chs.indexOf(socket.id) < 0) {
+                    peerChannels[peerId].push(socket.id);
+                }
+            }
 
-       }
+        }
 
     });
 
@@ -98,31 +97,51 @@ exports.handleNewChannel = function (socket) {
         var toRole = message.toRole;
         var affairId = message.affairId;
         var groupId = message.groupId;
-        if(msgType.indexOf("chat") == 0){
-            if(msgType.indexOf("chat") == 0) {
-                if (affairId == consts.friend_key) {  //如果affairId = 'friend'那么是朋友间聊天
-                    caches.ifPeerFriendRelation(fromRole, toRole).then(function (res) {
-                        if(res){ exports.sendMessageToPeer(message, toRole);}
-                    }, function (error) {
-
-                    });
-                } else {
-                    caches.ifPeerAffairRelation(fromRole, toRole, affairId)
-                        .then(function (res) {
-                        if (res) {//如果聊天的双发在同一个affair中
-                           exports.sendMessageToPeer(message, toRole);
-                        } else {
-                        //TODO 需要报错么
-                        }
-                        }, function (error) {
-
-                    });
-                }
-            }else if(msgType.indexOf("group")){ //如果发送的是群聊信息
-                exports.sendMessageToGroup(message, groupId);
+        if (msgType.indexOf("chat") == 0) {
+            if (affairId == consts.friend_key) {  //如果affairId = 'friend'那么是朋友间聊天
+                sendMessageToFriend(fromRole, toRole, message);
+            } else {
+                sendMessageToAffairPeer(fromRole, toRole, affairId, message);
+            }
+        } else if (msgType.indexOf('group')) { //如果发送的是群聊信息
+            exports.sendMessageToGroup(message, groupId);
+        } else if (msgType.indexOf('multi')) {
+            var toRoleIds = message.toRoleIds;
+            if (affairId == consts.friend_key) {  //如果affairId = 'friend' 那么是朋友聊天
+                _.each(toRoleIds, function (roleId) {
+                    sendMessageToFriend(fromRole, roleId, message);
+                });
+            } else {
+                _.each(toRoleIds, function (roleId) {
+                    sendMessageToAffairPeer(fromRole, roleId, affairId, message);
+                });
             }
         }
     });
+
+
+    function sendMessageToFriend(fromRole, toRole, message) {
+        caches.ifPeerFriendRelation(fromRole, toRole).then(function (res) {
+            if (res) {
+                exports.sendMessageToPeer(message, toRole);
+            }
+        }, function (error) {
+
+        });
+    };
+
+    function sendMessageToAffairPeer(fromRole, toRole, affairId, message) {
+        caches.ifPeerAffairRelation(fromRole, toRole, affairId)
+            .then(function (res) {
+                if (res) {//如果聊天的双发在同一个affair中
+                    exports.sendMessageToPeer(message, toRole);
+                } else {
+                    //TODO 需要报错么
+                }
+            }, function (error) {
+
+            });
+    };
 
     /**
      * 获得当前在连socket数量
@@ -144,9 +163,9 @@ exports.handleNewChannel = function (socket) {
     socket.on('peers_status', function (requestId, peers) {
         var result = [];
         _.each(peers, function (peer) {
-            if(_.isArray(peerChannels[peer] && peerChannels[peer].length > 0)){
+            if (_.isArray(peerChannels[peer] && peerChannels[peer].length > 0)) {
                 result.push(true);
-            }else {
+            } else {
                 result.push(false);
             }
         });
@@ -161,7 +180,7 @@ exports.handleNewChannel = function (socket) {
 };
 
 
-function isOnline(peerId){
+function isOnline(peerId) {
     return peerChannels.hasOwnProperty(peerId);
 };
 
@@ -173,7 +192,7 @@ exports.getOnlineChannelPeerIds = function () {
         var keys = _.chain(peerChannels).keys().filter(function (key) {
             //TODO 暂时不做任何筛选
         }).compact().unique().value();
-        if(keys.length > 50){
+        if (keys.length > 50) {
             keys = keys.subItems(0, 50);
         }
         resolve(keys);
@@ -181,12 +200,12 @@ exports.getOnlineChannelPeerIds = function () {
 };
 
 exports.clearChannel = function (ch) {
-    if(ch && ch.peerId && ch.id){
+    if (ch && ch.peerId && ch.id) {
         //第一步删除peerChannels中的信息
         var chs = peerChannels[ch.peerId];
-        if(_.isArray(chs)){
+        if (_.isArray(chs)) {
             chs.remove(ch.id);
-            if(chs.length < 1){
+            if (chs.length < 1) {
                 delete peerChannels[ch.peerId];
             }
         }
