81340e17670207362668539aa146f2d29b26cc85
nemo733 <915281733@qq.com>
2016-06-30 16:47:21 +0800

    messag push

---
+++ push/pushNotice/PushNoticeToAndroid.js
@@ -0,0 +1,20 @@
+var ALY = require('../aliyun-sdk-js/index.js');
+var push = new ALY.PUSH({
+        accessKeyId: 'I3L5ZpfjJM4Oamo5',
+        secretAccessKey: 'dexEw9DMZVymWF7WjiTdNFbtBU3Qg8',
+        endpoint: 'http://cloudpush.aliyuncs.com',
+        apiVersion: '2015-08-27'
+    }
+);
+push.pushNoticeToAndroid({
+    AppKey: '23396884',
+    Target: 'all', // 推送目标: device：推送给指定设备； account：推送给指定帐号；all：推送给全部
+    TargetValue: 'all',
+    Title: 'title',
+    Summary: 'fuck all of you in the black room',
+    AndroidExtParameters: '{\"id\":1002,\"content\":\"Hello OpenAPI!\"}'
+}, function (err, res) {
+    console.log(err, res);
+});
+
+return ;
