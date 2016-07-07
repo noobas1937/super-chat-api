a368108409ebdf9288746463584ed7074a0f50eb
xmc1993 <1170143845@qq.com>
2016-07-07 15:41:41 +0800

    服务器地址

--- realtime/consts.js
+++ realtime/consts.js
@@ -1,6 +1,6 @@
 var consts = {
-    mongo_uri: 'mongodb://simu:superid@localhost:27017/superChat',
-    redis_uri: 'redis://localhost:6379/0',
+    mongo_uri: 'mongodb://simu:superid@192.168.1.100:27017/superChat',
+    redis_uri: 'redis://192.168.1.100:6379/0',
     friend_key : 'friend'
 };
 
